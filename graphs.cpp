53086fc6c9da7f549c4badd756b7f272cb4cde3d
cw <10847666@qq.com>
2018-12-17 19:32:19 +0800

    graphs

---
+++ graphs.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+void graphmain()
+{
+	std::cout<<"---------------------------------------------------------\n";
+	std::cout<<"---------------------------------------------------------\n";
+	std::cout<<"---            简 易 工 资 管 理 系 统 v1.0 release     \n";
+	std::cout<<"---            1.部门信息                              \n";
+	std::cout<<"---            2.岗位信息及分级                         \n";
+	std::cout<<"---            3.员工基本信息                           \n";
+	std::cout<<"---            4.工资信息                              \n";
+	std::cout<<"---            5.退出系统                              \n";
+	std::cout<<"---            6.联系制作团队                    \n";
+	std::cout<<"---------------------------------------------------------\n";
+	std::cout<<"---------------------------------------------------------\n";
+}
+void makerinfo()
+{
+	std::cout<<"---            QQ群:861692730\n"<<std::endl;
+}
+#if 0
+int main(int argc, char const *argv[])
+{
+	graphmain();
+	int chooser;
+	std::cin>>chooser;
+	makerinfo();
+	return 0;
+}
+#endif
